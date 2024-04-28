e0881bfe22d0867265c5d4d59ca240c41855d57b
Cameron Stiver <cstiver@br013.ib.bridges2.psc.edu>
2024-04-28 17:41:33 -0400

    a

--- hw4-v2/pi2.cpp
+++ hw4-v2/pi2.cpp
@@ -49,35 +49,30 @@ int main(int argc, char** argv)
 
     MPI_Win win;
     if (world_rank == 0) {
-    	// Allocate memory for the global PI result at the master process
     	MPI_Alloc_mem(sizeof(long double), MPI_INFO_NULL, &global_PI_ptr);
-    	*global_PI_ptr = 3.0; // Initialize global PI
+    	*global_PI_ptr = 0.0; // Initialize global PI
     	MPI_Win_create(global_PI_ptr, sizeof(long double), sizeof(long double), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
     } else {
         // All other processes do not expose memory
-    	MPI_Win_create(NULL, 0, sizeof(long double), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    	MPI_Win_create(NULL, sizeof(long double), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
     }
 
     MPI_Win_fence(0, win); // Opening of the first RMA epoch
     if (world_rank == 0) {
-    	// Master process initializes its part of PI
     	*global_PI_ptr = calcPI(3.0, 2.0, 1, iterations_per_process);
     } else {
     	// Worker processes calculate their portion of PI
     	local_PI = calcPI(0.0, 2.0 + (world_rank * 2), (world_rank % 2 == 0 ? 1 : -1), iterations_per_process);
     	MPI_Put(&local_PI, 1, MPI_LONG_DOUBLE, 0, 0, 1, MPI_LONG_DOUBLE, win);
     }
-
+    MPI_Win_fence(0, win);
     if (world_rank == 0) {
-    	MPI_Win_fence(0, win); // Opening of the second RMA epoch
     	for (int i = 1; i < world_size; i++) {
         	// Use MPI_Get to retrieve the partial PI values from the workers
         	long double temp;
         	MPI_Get(&temp, 1, MPI_LONG_DOUBLE, i, 0, 1, MPI_LONG_DOUBLE, win);
         	*global_PI_ptr += temp;
     	}
-   	// Synchronize after all MPI_Get calls
-   	MPI_Win_fence(0, win);
     }
 
 	MPI_Win_free(&win);
