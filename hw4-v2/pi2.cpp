1fd153402f9d18c8e8eb972710333a4386669207
Cameron Stiver <cstiver@br013.ib.bridges2.psc.edu>
2024-04-28 17:29:29 -0400

    asff

--- hw4-v2/pi2.cpp
+++ hw4-v2/pi2.cpp
@@ -36,29 +36,40 @@ int main(int argc, char** argv)
     auto start = std::chrono::steady_clock::now();  // set timer
     long double PI = 3, n = 2, sign = 1;
     const long double PI25DT = 3.141592653589793238462643383; // set test value for error
+    
     if (argc==1)
     {
       printf("You must pass a single numeric value\n");
       printf("the value should be 100M or higher\n");
       return -1;
     }
-	MPI_Win win;
+
+    long double iterations = std::stod(argv[1]); // set to passed-in numeric value    
+    long double iterations_per_process = iterations / world_size;
+
+    MPI_Win win;
     if (world_rank == 0) {
-        // Allocate memory for the global PI result at the master process
-        MPI_Alloc_mem(sizeof(long double), MPI_INFO_NULL, &global_PI_ptr);
-        *global_PI_ptr = 3.0; // Initialize global PI
-        MPI_Win_create(global_PI_ptr, sizeof(long double), 1, MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    	// Allocate memory for the global PI result at the master process
+    	MPI_Alloc_mem(sizeof(long double), MPI_INFO_NULL, &global_PI_ptr);
+    	*global_PI_ptr = 3.0; // Initialize global PI
+    	MPI_Win_create(global_PI_ptr, sizeof(long double), sizeof(long double), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
     } else {
         // All other processes do not expose memory
-        MPI_Win_create(NULL, 0, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    	MPI_Win_create(NULL, 0, sizeof(long double), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
     }
-	MPI_Win_fence(0, win);
-    long double iterations = std::stod(argv[1]); // set to passed-in numeric value    
-	long double iterations_per_process = iterations / world_size;
 
+    MPI_Win_fence(0, win); // Opening of the first RMA epoch
     if (world_rank == 0) {
     	// Master process initializes its part of PI
     	*global_PI_ptr = calcPI(3.0, 2.0, 1, iterations_per_process);
+    } else {
+    	// Worker processes calculate their portion of PI
+    	local_PI = calcPI(0.0, 2.0 + (world_rank * 2), (world_rank % 2 == 0 ? 1 : -1), iterations_per_process);
+    	MPI_Put(&local_PI, 1, MPI_LONG_DOUBLE, 0, 0, 1, MPI_LONG_DOUBLE, win);
+    }
+
+    if (world_rank == 0) {
+    	MPI_Win_fence(0, win); // Opening of the second RMA epoch
     	for (int i = 1; i < world_size; i++) {
         	// Use MPI_Get to retrieve the partial PI values from the workers
         	long double temp;
@@ -66,15 +77,6 @@ int main(int argc, char** argv)
         	*global_PI_ptr += temp;
     	}
    	// Synchronize after all MPI_Get calls
-   	MPI_Win_fence(0, win);
-   	// Compute the final value of PI
-   	*global_PI_ptr += 3.0 * (world_size - 1); // Add the starting value of PI from each worker
-    } else {
-   	// Worker processes calculate their portion of PI
-   	local_PI = calcPI(0.0, 2.0 + (world_rank * 2), (world_rank % 2 == 0 ? 1 : -1), iterations_per_process);
-   	// Use MPI_Put to send the local PI calculation to the master's window
-   	MPI_Put(&local_PI, 1, MPI_LONG_DOUBLE, 0, 0, 1, MPI_LONG_DOUBLE, win);
-   	// Synchronize after MPI_Put
    	MPI_Win_fence(0, win);
     }
 
